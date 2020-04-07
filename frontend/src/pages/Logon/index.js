ac01816f4b5780c34a655eba7448549d91d90d5c
Felipe Silveira <eng.fe.silveira@gmail.com>
2020-04-06 22:06:08 -0400

    frontend login working with backend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'; //father icons
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const res = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', res.data.name);
+
+            history.push('/profile');
+        }catch(err){
+            alert('Login failed, try again.')
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
-                <img src={logoImg} alt="Be The Hero"/>
+                <img src={logoImg} alt="Be The Hero" />
 
-                <form action="">
+                <form onSubmit={handleLogin}>
                     <h1>Do your logon</h1>
 
-                    <input placeholder="Your ID"/>
+                    <input
+                        placeholder="Your ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Login</button>
 
                     <Link className="back-link" to="/register">
@@ -26,7 +49,7 @@ export default function Logon() {
                 </form>
             </section>
 
-            <img src={heroesImg} alt="Heroes"/>
+            <img src={heroesImg} alt="Heroes" />
         </div>
     );
 }
